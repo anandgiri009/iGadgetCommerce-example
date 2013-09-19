251a772f75f2ba9f42f375f83189942f18883398
Naseem Makiya <naseem.makiya@moovweb.com>
2013-09-19 16:25:31 -0700

    merge conflict reso

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,9 +1,13 @@
 $("./body") {
   add_class("mw_home")
-  $$("#HomeFeaturedProducts") {
+  $$("#HomeFeaturedProducts, #HomeNewProducts") {
     ur_carousel("..//li[contains(@class, 'Odd') or contains(@class, 'Even')]")
     remove(css(".BlockContent"))
+    attribute("data-ur-center", "enabled")
+    attribute("data-ur-autoscroll", "enabled")
+    attribute("data-ur-touch", "enabled")
+    $$("[data-ur-carousel-component='count'], [data-ur-carousel-button-type='next'], [data-ur-carousel-button-type='prev']") {
+      remove()
+    }
   }
-
-  
 }
