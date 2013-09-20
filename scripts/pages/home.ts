4efcc3926a15f2e7d34fbd6b96275597ec2ff354
Naseem Makiya <naseem.makiya@moovweb.com>
2013-09-20 14:40:32 -0700

    cleaning up stuff

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -4,8 +4,8 @@ $("./body") {
   $$("#HomeFeaturedProducts, #HomeNewProducts") {
     ur_carousel("..//li[contains(@class, 'Odd') or contains(@class, 'Even')]")
     remove(css(".BlockContent"))
-    attribute("data-ur-center", "enabled")
-    attribute("data-ur-autoscroll", "enabled")
+    attribute("data-ur-center", "disabled")
+    attribute("data-ur-autoscroll", "disabled")
     attribute("data-ur-touch", "enabled")
     $$("[data-ur-carousel-component='count'], [data-ur-carousel-button-type='next'], [data-ur-carousel-button-type='prev']") {
       remove()
