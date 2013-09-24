fd7ea98875cd0b2d87b362a5d80a978629b35bdd
Naseem Makiya <naseem.makiya@moovweb.com>
2013-09-23 17:14:14 -0700

    merge conflict reso

--- scripts/pages/category.ts
+++ scripts/pages/category.ts
@@ -2,70 +2,53 @@ $("./body") {
   add_class("mw_category")
   
   $("./div[@id='Container']") {
-        
     $("./div[@id='Outer']") {
       $("./div[@id='Wrapper']") {
-        
         $("./div[@class='Left']") {
+
+          # Create Togglers
           $("./div[@id='SideCategoryShopByPrice']") {
-            # Toggler container
             attribute("data-ur-set", "toggler")
-            
-            # Toggler button
             $("./h2") {
               attribute("data-ur-toggler-component", "button")
               insert("div", class: "mw_indicator")
             }
-            
-            # Toggler content
             $("./div") {
               attribute("data-ur-toggler-component", "content")
-            }
-                        
+            }           
           }
           $("./div[@id='SideShopByBrand']") {
-            # Toggler container
             attribute("data-ur-set", "toggler")
-            
-            # Toggler button
             $("./h2") {
               attribute("data-ur-toggler-component", "button")
               insert("div", class: "mw_indicator")
             }
-            
-            # Toggler content
             $("./div") {
               attribute("data-ur-toggler-component", "content")
-            }
-                        
+            }          
           }
+
           $(".//div[contains(@class, 'NewsletterSubscription')]") {
             remove()
           }
         }
-        
+
         $("./div[@class='Content ']") {
           $("./div[@id='CategoryHeading']") {
             $("./div[@class='BlockContent']") {
-              
               # Move page title on top of the content area
               $("./h2") {
                 move_to("ancestor::div[@id = 'Wrapper']/div[@class='Left']", "top")
               } 
-              
               $("./div[@class='FloatRight SortBox']") {     
                 $("./form") {
-                  # Wrap "Sort by"" text with <span> tag"
                   wrap_text_children("span")
                 }       
               }
-            
             }
           }
         }
-        
       }
     }
-  }
-  
+  } 
 }
