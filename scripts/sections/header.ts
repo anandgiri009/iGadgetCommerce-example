455317282659632f9a9baf3753f06452fef9a35c
Naseem Makiya <naseem.makiya@moovweb.com>
2013-09-26 14:48:46 -0700

    fixing stuff

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -2,40 +2,39 @@ $('./body') {
   $(".//div[@id='Header']") {
     attribute("data-ur-set", "toggler")
     
-      $("./div[@id='Logo']") {
+    $("./div[@id='Logo']") {
       # Move top menu into #Logo container
       move_here("../../../div[@id='TopMenu']", "bottom"){
         # Remove unwanted top menu items from upper menu
         remove(".//li[not(contains(@class, 'CartLink') or contains(@class, 'First'))]")
       }
-
-      # Insert HTML for bottom of header
-      insert_after("div", class:"mw_header_bottom") {
-        insert("div", class:"mw_search")
-        insert("div", class:"mw_menu_btn", data-ur-toggler-component: "button")
-      }
-
-      # Fill search HTML
-      $("..//div[@class='mw_search']"){
-        # Move search form into container element
-        move_here("//div[@id='SearchForm']", "bottom"){
-          remove("./p")
-
-          $("./form") {
-            remove("./label")
-            # Change search button background image
-            $("./input[@type='image']") {
-              wrap("div", class: "mw_search_btn sprites-search")
-            }
-            $("./input[@type='text']") {
-              attribute("placeholder", "Search…")
-            } 
-          }     
-        }
-      }   
     }
-    remove("./br")
 
+    # Insert HTML for bottom of header
+    insert("div", class:"mw_header_bottom") {
+      insert("div", class:"mw_search")
+      insert("div", class:"mw_menu_btn", data-ur-toggler-component: "button")
+    }
+
+    # Fill search HTML
+    $("..//div[@class='mw_search']"){
+      # Move search form into container element
+      move_here("//div[@id='SearchForm']", "bottom"){
+        remove("./p")
+
+        $("./form") {
+          remove("./label")
+          # Change search button background image
+          $("./input[@type='image']") {
+            wrap("div", class: "mw_search_btn sprites-search")
+          }
+          $("./input[@type='text']") {
+            attribute("placeholder", "Search…")
+          } 
+        }     
+      }
+    }   
+    remove("./br")
     # Move menu items into header
     move_here("../div[@id='Menu']", "bottom"){
       attribute("data-ur-toggler-component", "content")
