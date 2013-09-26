afe29a7b374aa1e0d5e6e4110deebef9018a05f0
Naseem Makiya <naseem.makiya@moovweb.com>
2013-09-26 15:44:55 -0700

    commented out code to walk through

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,16 +1,16 @@
-$("./body") {
-  add_class("mw_home")
+# $("./body") {
+#   add_class("mw_home")
 
-  # Creating Image Carousels
-  $$("#HomeFeaturedProducts, #HomeNewProducts") {
-    ur_carousel("..//li[contains(@class, 'Odd') or contains(@class, 'Even')]")
-    remove(css(".BlockContent"))
-    attribute("data-ur-center", "disabled")
-    attribute("data-ur-autoscroll", "enabled")
-    attribute("data-ur-touch", "enabled")
-  }
+#   # Creating Image Carousels
+#   $$("#HomeFeaturedProducts, #HomeNewProducts") {
+#     ur_carousel("..//li[contains(@class, 'Odd') or contains(@class, 'Even')]")
+#     remove(css(".BlockContent"))
+#     attribute("data-ur-center", "disabled")
+#     attribute("data-ur-autoscroll", "enabled")
+#     attribute("data-ur-touch", "enabled")
+#   }
 
-  # Remove unnecessary content
-  remove(".//span[contains(@class, 'FeedLink')]")
-  remove(".//div[@id='LayoutColumn3']")
-}
+#   # Remove unnecessary content
+#   remove(".//span[contains(@class, 'FeedLink')]")
+#   remove(".//div[@id='LayoutColumn3']")
+# }
