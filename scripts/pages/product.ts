88dfe591f60dc21df210ada2051063a838dd0555
Naseem Makiya <naseem.makiya@moovweb.com>
2013-09-30 12:01:21 -0700

    finishing up product page

--- scripts/pages/product.ts
+++ scripts/pages/product.ts
@@ -1,8 +1,15 @@
 $("/html/body") {
   add_class("_product")
   
-  $$("#LayoutColumn1, #LayoutColumn3, #ImageScrollPrev, #ImageScrollNext, #SimilarProductsByCustomerViews, #ProductBreadcrumb") {
+  $$("#LayoutColumn1, #LayoutColumn3, #ImageScrollPrev, #ImageScrollNext, #SimilarProductsByCustomerViews, #ProductBreadcrumb, #ProductReviews > a") {
     remove()
+  }
+
+  $$(".AddCartButton") {
+    insert("a", "Add to Cart", class:"mw_btn1")
+    $$("input[type='image']") {
+      move_to("..", "./a")
+    }
   }
 }
 
