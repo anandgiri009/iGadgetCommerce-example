9b7032a122acd755df9e6292c128a37183479d6e
Naseem Makiya <naseem.makiya@moovweb.com>
2013-10-22 15:21:40 -0700

    adding assertion mixer and some assertions

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,14 +1,18 @@
 $("./body") {
   add_class("mw_home")
 
+  # Notice this is broken on mlocal.mstage.igadgetcommerce.com
   # Creating Image Carousels
-  $$("#HomeFeaturedProducts, #HomeNewProducts") {
+  must_$("//*[@id='HomeFeaturedProducts'] | //*[@id='HomeNewProducts']") {
     ur_carousel("..//li[contains(@class, 'Odd') or contains(@class, 'Even')]")
     remove(css(".BlockContent"))
     attribute("data-ur-center", "disabled")
     attribute("data-ur-autoscroll", "enabled")
     attribute("data-ur-touch", "enabled")
   }
+
+  log("Failure Count: " + $ASSERTION_FAILURE_COUNT)
+  log("Failure Messages: " + $ASSERTION_FAILURE_MESSAGES)
 
   # Remove unnecessary content
   remove(".//span[contains(@class, 'FeedLink')]")
