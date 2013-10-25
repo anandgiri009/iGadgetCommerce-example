fa5568c9597f405c89b3587c6b5ae8183886feb1
Naseem Makiya <naseem.makiya@moovweb.com>
2013-10-25 15:42:58 -0700

    fixing selector again

--- scripts/asserts/home.ts
+++ scripts/asserts/home.ts
@@ -1,6 +1,6 @@
 # Homepage Assertions
 
-$("//*[@id='HomeNewProducts'] | //*[@id='HomeFeaturedProducts']") {
+must_$("//*[@id='HomeNewProducts'] | //*[@id='HomeFeaturedProducts']") {
   has_attr("data-ur-set", "carousel")
   must_$(".//div[@data-ur-carousel-component='scroll_container']") {  
     must_$("..//li[contains(@class, 'Odd') or contains(@class, 'Even')]") {
