49a1fd7fc7b21142ed0f664bd2f086b5442bafa6
Tevfik Sertel <tsertel@gmail.com>
2014-02-28 16:20:33 -0600

    fixing my account

--- scripts/ajax/account.ts
+++ scripts/ajax/account.ts
@@ -1,7 +1,7 @@
 $("./body") {
   add_class("mw-account-iframe-content")
   $("./div[@id = 'Container']"){
-    move_here(".//div[@id = 'LoginForm']", "before") {
+    move_here(".//div[@id = 'LayoutColumn2']", "before") {
       
     }
     remove()
