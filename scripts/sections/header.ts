934002236bd87d98f30b6709e8b34ece69c40a5d
Tevfik Sertel <tsertel@gmail.com>
2014-02-28 16:49:32 -0600

    Revert "merge to be removed"
    This reverts commit b7776a237a59513fce732ebed151709c83e9dc5d.

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,5 +1,4 @@
 $('./body') {
-  add_class("mw_header")
   $(".//div[@id='Header']") {
       # Move top menu into #Logo container
       $("./div[@id='Logo']") {
@@ -40,11 +39,7 @@ $('./body') {
     }                       
     # Remove br
     remove("./br")
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 90bc4b9... demo code complete
     # Move menu items into header
     move_here("../div[@id='Menu']", "bottom"){
       # Move "Category List" element on bottom of #Menu element
