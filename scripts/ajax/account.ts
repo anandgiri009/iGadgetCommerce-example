b874d1ececae0888a6fb8db36ca27980d74a1f7a
Tevfik Sertel <tsertel@gmail.com>
2014-02-28 18:21:26 -0600

    account section (login)

--- scripts/ajax/account.ts
+++ scripts/ajax/account.ts
@@ -1,8 +1,34 @@
 $("./body") {
-  add_class("mw-account-iframe-content")
+  add_class("mw-account-content")
   $("./div[@id = 'Container']"){
     move_here(".//div[@id = 'LayoutColumn2']", "before") {
-      
+      remove("./div[contains(@class, 'Breadcrumb')]")
+      $("./div[@id = 'LoginForm']") {
+        remove(".//p[contains(@class, 'SuccessMessage')]")
+        $(".//div[@class = 'AccountLogin']") {
+          # Create account section
+          move_here("preceding-sibling::div[@class = 'CreateAccount']", "after") {
+            inner_wrap("to_be_removed") {
+              move_here(".//a", "before") {
+                text("Create Account")
+                add_class("mw-btn1")
+              }
+              remove()
+            }
+            insert_top("div", class: "mw-or", "Or")
+          }
+          # Login section
+          $(".//input[@id = 'LoginButton']") {
+            add_class("mw-btn1")
+          }
+          $(".//a[contains(@href, 'reset_password')]") {
+            add_class("mw-forgot-password-btn")
+          }
+        }
+      }
+      $(".//h2") {
+        add_class("mw-h2")
+      }
     }
     remove()
   }
